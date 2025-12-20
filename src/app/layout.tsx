4cdd2998bf529e19d936f0183f2dd420aadd3446
iago bruno <iago.silva6969@gmail.com>
2025-12-20 18:23:17 -0300

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,11 +12,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="pt-BR">
+      <body className="w-full bg-whiteTheme overflow-hidden">
+        <main>
+          <div
+            className="grid grid-cols-[250px_1fr] gap-5 p-4 min-h-screen
+      "
+          >
+            <section className="bg-contentTheme rounded-2xl shadow-2xl p-4">
+              section 1
+            </section>
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   );
